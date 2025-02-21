4cf6abf447115765eb867bdc86fac7b46293fe25
Isaac De La Lama <isaacdll99@gmail.com>
2025-02-20 18:21:38 -0600

    added manifest and apple meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -39,6 +39,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
       </head>
       <body className="m-0 p-0 h-screen flex flex-col">
         <Header />
